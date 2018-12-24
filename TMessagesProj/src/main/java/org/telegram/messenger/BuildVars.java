7c782d6688effb7b83ea4f0ccc7d900942dffe8e
Alexander Baulin <aleks.baulin@gmail.com>
2018-12-24 12:54:25 +0300

    build & run ok

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -19,10 +19,10 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = false;
     public static int BUILD_VERSION = 1358;
     public static String BUILD_VERSION_STRING = "4.9.1";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int APP_ID = 138016; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "eddc78680ccae7c412c806d1e7137bb1"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "eddc78680ccae7c412c806d1e7137bb1";
+    public static String HOCKEY_APP_HASH_DEBUG = "eddc78680ccae7c412c806d1e7137bb1";
     public static String PLAYSTORE_APP_URL = "";
 
     static {
